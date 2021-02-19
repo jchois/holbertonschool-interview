29c23ea954c483e699991424fd8bc0f15c76c365
jchois <choisjuliana@gmail.com>
2021-02-18 23:39:26 -0500

    [Fix]

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
 import math
 
 
-def minOperations(n):
-    """ Calculates the fewest number of operations needed to result in
-        exactly n H characters
-    """
-    sum = 0
-    if n <= 1:
-        return sum
-    for i in range(2, int(math.sqrt(n) + 1)):
+def factors(n):
+    """factors of n number"""
+    mylist = []
+    while n % 2 == 0:
+        mylist.append(2)
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         while n % i == 0:
-            sum += i
-            n = n // i
-    if n > 1:
-        sum += n
-    return sum
+            mylist.append(i)
+            n = n / i
+    if n > 2:
+        mylist.append(n)
+    return mylist
+
+
+def minOperations(n):
+    """calculate the minimum operations"""
+    if type(n) != int or n < 2:
+        return 0
+    else:
+        numOperations = sum(factors(n))
+        return int(numOperations)
